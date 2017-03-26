ab2494d5c9d2e3c1f8dbd7d2e65266a2db6bac6a
Grégory Beurier <gregory.beurier@cirad.fr>
2017-03-26 18:13:41 +0200

    Fixed sources

---
+++ src/ecomeristem/version.hpp
@@ -0,0 +1,37 @@
+/**
+ * @file version.hpp.in
+ * @author The Ecomeristem Development Team
+ * See the AUTHORS file
+ */
+
+/*
+ * Copyright (C) 2005-2015 Cirad http://www.cirad.fr
+ * Copyright (C) 2014 ULCO http://www.univ-littoral.fr
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef ECOMERISTEM_VERSION_HPP
+#define ECOMERISTEM_VERSION_HPP 1
+
+#define ECOMERISTEM_MAJOR_VERSION   (1)
+#define ECOMERISTEM_MINOR_VERSION   (0)
+#define ECOMERISTEM_PATCH_VERSION   (0)
+#define ECOMERISTEM_EXTRA_VERSION   ""
+#define ECOMERISTEM_VERSION         "1.0.0"
+#define ECOMERISTEM_ABI_VERSION     "1.0"
+#define ECOMERISTEM_NAME_COMPLETE   "ecomeristem-1.0.0"
+#define ECOMERISTEM_LOCALE_NAME     "ecomeristem-1-0"
+
+#endif
